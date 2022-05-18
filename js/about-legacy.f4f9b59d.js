9eb049b879edac17d91e8272b71e84e1bae2cdec
FelipeUribe81 <felipe.uribe@correo.usa.edu.co>
2022-05-17 23:41:48 -0500

    Deployment

---
+++ js/about-legacy.f4f9b59d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_usa_comex"]=self["webpackChunkvue_usa_comex"]||[]).push([[443],{15830:function(e,t,u){u.r(t),u.d(t,{default:function(){return r}});var n=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,u=e._self._c||t;return u("div",{staticClass:"about"},[u("h1",[e._v("This is an about page")])])}],a=u(1001),c={},l=(0,a.Z)(c,n,s,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about-legacy.f4f9b59d.js.map
