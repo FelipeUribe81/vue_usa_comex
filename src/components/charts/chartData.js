05e19c0ea2ff3bb5108238959bf998bedf7867f4
Carlos Mariottiz <felipe.uribe@correo.usa.edu.co>
2022-09-03 22:09:39 -0500

    Each value of dataset has it s  sp eprtphas its percentage

--- src/components/charts/chartData.js
+++ src/components/charts/chartData.js
@@ -11,7 +11,7 @@ export function doughnut_or_pie_data(chartLabels, charData, charType) {
                 backgroundColor: backgroundColor,
                 borderColor: borderColor,
                 borderWidth: 1,
-            }, ],
+            },],
         },
         options: {
             responsive: true,
@@ -19,6 +19,19 @@ export function doughnut_or_pie_data(chartLabels, charData, charType) {
                 legend: {
                     backgroundColor: "#000000",
                     display: false,
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function (context) {
+                            var label = context.label,
+                                currentValue = context.raw,
+                                total = context.chart._metasets[context.datasetIndex].total;
+
+                            var percentage = parseFloat((currentValue / total * 100).toFixed(1));
+
+                            return label + ": " + currentValue + ' (' + percentage + '%)';
+                        }
+                    }
                 },
             },
         },
@@ -36,20 +49,35 @@ export function bar_or_line_data(chartLabels, charData, charType, chartLabel) {
                 backgroundColor: backgroundColor,
                 borderColor: borderColor,
                 borderWidth: 1,
-            }, ],
+            },],
         },
         options: {
             responsive: true,
             plugins: {
                 legend: {
                     display: false,
-                }
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function (context) {
+                            var 
+                                currentValue = context.raw,
+                                total = context.chart.data.datasets[0].data.reduce((accumulator, value) => {
+                                    return accumulator + value;
+                                  }, 0);
+                                    
+                            var percentage = parseFloat((currentValue / total * 100).toFixed(1));
+
+                            return chartLabel + ": " + currentValue + ' (' + percentage + '%)';
+                        }
+                    }
+                },
             },
             scales: {
                 x: {
                     ticks: {
                         // For a category axis, the val is the index so the lookup via getLabelForValue is needed
-                        callback: function(val) {
+                        callback: function (val) {
                             // Hide the label of every 2nd dataset
                             return this.getLabelForValue(val).length > 5 ?
                                 String(this.getLabelForValue(val)).substring(0, 10) + "..." :
